e011d8cc9822a32e48a703799052c05b93b4fae8
cwt <cuiwanting@example.com>
2020-06-20 20:44:46 +0800

    1244

--- src/main/java/com/cuiwanting/test/Hello.java
+++ src/main/java/com/cuiwanting/test/Hello.java
@@ -6,5 +6,8 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("helloWordhghg");
 		System.out.println("helloWordhghg");
+		System.out.println("helloWordhghg");
+		System.out.println("helloWordhghg");
+		System.out.println("helloWordhghg");
 	}
 }
