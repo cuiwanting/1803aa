79f9589fb63f5a451a36a9834ec4ad8a4f15a4e5
cwt <cuiwanting@example.com>
2020-06-21 20:38:31 +0800

    ...........

--- src/main/java/com/cuiwanting/test/Hello.java
+++ src/main/java/com/cuiwanting/test/Hello.java
@@ -5,8 +5,10 @@ public class Hello {
 	
 	public static void main(String[] args) {
 		System.out.println("helloWordhghg");
+		System.out.println("helloWordhghg");
 		
 		String str="123";
 		System.out.println(str);
+		
 	}
 }
